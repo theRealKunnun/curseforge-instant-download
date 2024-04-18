796d9e427d55265839c7d6a61e0fe988e099ca1d
elias <elias>
2024-04-19 01:02:15 +0200

    structure

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,32 +1,28 @@
-// tabs
-chrome.tabs.onActivated.addListener(
-    function (activeInfo){
-        console.debug('onActivated');
-        console.debug(activeInfo);
-    }
-)
-
-chrome.tabs.onHighlighted.addListener(
-    function (highlightInfo){
-        console.debug('onHighlighted');
-        console.debug(highlightInfo);
-    }
-)
-
-chrome.tabs.onUpdated.addListener(
-    function (tabId,changeInfo,tab){
-        console.debug('onUpdated');
-        console.debug(tabId);
-        console.debug(changeInfo);
-        console.debug(tab);
-    }
-)
+// // tabs
+// chrome.tabs.onActivated.addListener(function (activeInfo) {
+//     console.debug('onActivated');
+//     console.debug(activeInfo);
+// })
+//
+// chrome.tabs.onHighlighted.addListener(function (highlightInfo) {
+//     console.debug('onHighlighted');
+//     console.debug(highlightInfo);
+// })
+//
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     console.debug('onUpdated');
+//     console.debug(tabId);
+//     console.debug(changeInfo);
+//     console.debug(tab);
+// })
 
 // webRequest
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details){
-        console.debug('onBeforeRequest');
-        console.debug(details);
-    },
-    {urls: ['https://www.curseforge.com/*']}
-)
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+    console.debug('onBeforeRequest');
+    console.debug(details);
+}, {urls: ['https://*.curseforge.com/*']})
+
+// chrome.webRequest.onBeforeRedirect.addListener(function (details) {
+//     console.debug('onBeforeRedirect');
+//     console.debug(details);
+// }, {urls: ['https://*.curseforge.com/*']})
