31cfddd4f3482e88eb0a17f1e9f4d5e98880370f
elias <elias>
2024-04-19 00:37:55 +0200

    added listeners

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,121 +1,32 @@
-//content script
-//go with service worker -> manual
+// tabs
+chrome.tabs.onActivated.addListener(
+    function (activeInfo){
+        console.debug('onActivated');
+        console.debug(activeInfo);
+    }
+)
 
-function generateRandomId() {
-    return new Promise(function (resolve) {
-        const number = Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
-        resolve(number);
-    })
-}
-function getFileIdFromUrl(url) {
-    return new Promise(function (resolve) {
-        const parts = url.split('/');
-        resolve(parts[parts.length - 1])
-    })
-}
+chrome.tabs.onHighlighted.addListener(
+    function (highlightInfo){
+        console.debug('onHighlighted');
+        console.debug(highlightInfo);
+    }
+)
 
-//https://www.curseforge.com/minecraft/mc-mods/yungs-better-ocean-monuments/download/5124323
-//https://www.curseforge.com/api/v1/mods/689238/files/5124323/download
-chrome.tabs.onActivated.addListener(async function (activeInfo) {
+chrome.tabs.onUpdated.addListener(
+    function (tabId,changeInfo,tab){
+        console.debug('onUpdated');
+        console.debug(tabId);
+        console.debug(changeInfo);
+        console.debug(tab);
+    }
+)
 
-
-
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const modId = await chrome.tabs.sendMessage(tab.id, "active tab changed");
-    console.debug(modId)
-
-    //create new custom redirect rule
-    await chrome.declarativeNetRequest.updateSessionRules({
-        addRules: [{
-            "id": await generateRandomId(), "priority": 1, "action": {
-                "type": "redirect",
-                "redirect": {"regexSubstitution": `https://www.curseforge.com/api/v1/mods/${modId}/files/\\4/download`}
-            }, "condition": {
-                "regexFilter": "^https:\\/\\/www\\.curseforge\\.com\\/([^\\/]+)\\/([^\\/]+)\\/([^\\/]+)\\/download\\/([^\\/]+)$",
-                "resourceTypes": ["main_frame"]
-            }
-        }]
-    });
-    //https://www.curseforge.com/_next/data/qcfOenDtpgFbDf2z54Dsc/en/minecraft/mc-mods/industrial-foregoing/download/5240372.json?slug=minecraft&classSlug=mc-mods&projectSlug=industrial-foregoing&fileId=5240372
-    //https://www.curseforge.com/minecraft/mc-mods/industrial-foregoing/download/5240372
-    // chrome.webRequest.onResponseStarted.addListener(function (details) {
-    //     const params = new URLSearchParams(new URL(details.url).search);
-    //     const fileId = params.get('fileId');
-    //
-    //     chrome.downloads.search(
-    //         {},
-    //         function (results){
-    //             console.log(results)
-    //         }
-    //     )
-    //
-    //
-    //     //let count = await chrome.storage.session.get('count');
-    //     // chrome.storage.session.get(["count"]).then((result) => {
-    //     //     if (result.count === 0) {
-    //     //         console.debug("good")
-    //     //         chrome.downloads.download({url: `https://www.curseforge.com/api/v1/mods/${modId}/files/${fileId}/download`}, function () {
-    //     //             console.log("downloading")
-    //     //         })
-    //     //         chrome.storage.session.set({count: 1})
-    //     //     } else {
-    //     //         console.debug("bad")
-    //     //     }
-    //     // });
-    //
-    //     // chrome.storage.session.get(["count"]).then((result) => {
-    //     //     if (result.modId !== modId) {
-    //     //         (async () => {
-    //     //             const response = await chrome.runtime.sendMessage({greeting: modId});
-    //     //         })();
-    //     //     }
-    //     // });
-    //
-    //     // if (count === 0) {
-    //     //     chrome.downloads.download({url: `https://www.curseforge.com/api/v1/mods/${modId}/files/${fileId}/download`}, function () {
-    //     //         count += 1
-    //     //     })
-    //     // }
-    //
-    //     // chrome.tabs.update({url: `https://www.curseforge.com/api/v1/mods/${modId}/files/${fileId}/download`}, async function () {
-    //     //     chrome.webRequest.onCompleted.addListener(async function () {
-    //     //         await chrome.tabs.sendMessage(tab.id, "go back");
-    //     //     }, {urls: ["https://www.curseforge.com/_next/data/*"]})
-    //     // })
-    // }, {urls: ["https://www.curseforge.com/*/*/*/download/*"]})
-    // chrome.webRequest.onBeforeRequest.addListener(async function () {
-    //     //maybe put in tab id from 'tab', because updateProperties has a 'tabId' sub-property
-    //     const [adTab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    //     console.debug(adTab.url);
-    //
-    //     //https://www.curseforge.com/api/v1/mods/266515/files/dependencies/download
-    //     // chrome.tabs.update({url: `https://www.curseforge.com/api/v1/mods/${response}/files/${await getFileIdFromUrl(adTab.url)}/download`}, function (tab) {
-    //     //
-    //     // })
-    // }, {
-    //     urls: ["https://www.curseforge.com/*/*/*/download/*"]
-    // })
-    // if (await adDownload(tab.url) === true){
-    //     console.debug("adDownload")
-    // } else {
-    //     console.debug("NOT adDownload")
-    // }
-})
-//, {
-//             "id": await generateRandomId(), "priority": 1, "action": {
-//                 "type": "redirect",
-//                 "redirect": {"regexSubstitution": `https://www.curseforge.com/api/v1/mods/${response}/files/\\1/download`}
-//             }, "condition": {
-//                 "regexFilter": "^https:\\/\\/www\\.curseforge\\.com\\/\\_next\\/data\\/.+\\=([\\d]+)$",
-//                 "resourceTypes": ["main_frame"]
-//             }
-//         }, {
-//             "id": await generateRandomId(), "priority": 1, "action": {
-//                 "type": "redirect",
-//                 "redirect": {"regexSubstitution": `https://www.curseforge.com/api/v1/mods/${response}/files/888/download`}
-//             }, "condition": {
-//                 "regexFilter": "^https:\\/\\/static-beta\\.curseforge\\.com\\/_next\\/static\\/chunks\\/pages\\/[\\D|\\d]+$",
-//                 "resourceTypes": ["main_frame"]
-//             }
-//         }
-
+// webRequest
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details){
+        console.debug('onBeforeRequest');
+        console.debug(details);
+    },
+    {urls: ['https://www.curseforge.com/*']}
+)
