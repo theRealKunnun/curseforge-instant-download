509bac4b29eb03707b4fbed8686f188ad62e4f19
elias <elias>
2024-04-19 06:05:42 +0200

    feature: 'instant download over widget' done

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,9 +1,64 @@
+let handleWidgetAppearance = () => {
+    let fileId;
+    const card = document.querySelector("#__next > main > div.container.project-page > div.modal-container > section > div.modal-content > div.download-details.is-latest > a");
+    if (card) {
+        const link = card.getAttribute("href").split("/");
+        fileId = link[link.length - 1];
+        console.debug(`file id found: ${fileId}`);
+    }
 
+    const widgetDownloadButton = document.querySelector("#__next > main > div.container.project-page > div.modal-container > section > div.actions > button");
+    if (widgetDownloadButton + fileId) {
+        console.debug(`widget download button found: ${widgetDownloadButton.textContent.trim()}`);
+        widgetDownloadButton.addEventListener('click', function () {
+            const modpageUrl = window.location.href;
+            console.debug(`widget download button clicked`);
+            chrome.storage.session.get(["idfc_modId"]).then((result) => {
+                console.debug(`mod id retrieved from storage: ${result.idfc_modId}`);
+                (async () => {
+                    const response = await chrome.runtime.sendMessage({reason: "download", data: { url: `https://www.curseforge.com/api/v1/mods/${result.idfc_modId}/files/${fileId}/download`}});
+                    console.debug(response);
+                    window.location.href = modpageUrl;
+                })();
+            });
+        });
+        mutationObserver.disconnect();
+    }
+};
 
-let modId = document.querySelector("#__next > main > div.container.project-page > aside > div.aside-box.project-details-box > section:nth-child(2) > dl > dd:nth-child(6)");
+const modId = document.querySelector("#__next > main > div.container.project-page > aside > div.aside-box.project-details-box > section:nth-child(2) > dl > dd:nth-child(6)").textContent.trim()
 if (modId) {
-    console.debug(modId.textContent.trim());
+    chrome.storage.session.set({idfc_modId: modId}).then(() => {
+        console.debug(`mod id was stored: ${modId}`);
+    });
 }
+
+const widgetDownloadButtonParentElement = document.querySelector("#__next > main > div.container.project-page");
+if (widgetDownloadButtonParentElement) {
+    console.debug("widgetDownloadButtonParentElement found");
+}
+
+let mutationObserverConfig = {childList: true, subtree: true}
+
+let callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            handleWidgetAppearance();
+        }
+    }
+};
+let mutationObserver = new MutationObserver(callback);
+mutationObserver.observe(widgetDownloadButtonParentElement, mutationObserverConfig);
+//mutationObserver.disconnect();
+
+
+// chrome.storage.session.set({ key: value }).then(() => {
+//     console.log("Value was set");
+// });
+//
+// chrome.storage.session.get(["key"]).then((result) => {
+//     console.log("Value is " + result.key);
+// });
 
 
 //
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,20 @@
+chrome.storage.session.setAccessLevel({accessLevel: "TRUSTED_AND_UNTRUSTED_CONTEXTS"}, function () {
+    console.debug("storage access level changed")
+})
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    //{reason: "download", data: { url: `https://www.curseforge.com/api/v1/mods/${result.idfc_modId}/files/${fileId}/download`}}
+    if (message.reason === "download") {
+        chrome.downloads.download(
+            {url: message.data.url},
+            function (downloadId) {
+                console.debug(`downloaded: ${message.data.url}`);
+            }
+        )
+        sendResponse("download started")
+    }
+})
+
 // // tabs
 // chrome.tabs.onActivated.addListener(function (activeInfo) {
 //     console.debug('onActivated');
