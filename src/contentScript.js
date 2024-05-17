02139949b7df7e5c680718a88f1ba643a4fdfc65
elias <elias>
2024-05-18 01:32:20 +0200

    added media plus comments

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,11 +1,14 @@
 let handleWidgetAppearance = () => {
     let fileId;
-    const card = document.querySelector("#__next > main > div.container.project-page > div.modal-container > section > div.modal-content > div.download-details.is-latest > a");
+    console.debug("trying....");
+    const card = document.querySelector("#__next > div > main > div.container.project-page > div.modal-container > section > div.modal-content > div.download-details.is-latest > a");
     if (card) {
         const link = card.getAttribute("href").split("/");
+        console.debug(`${link}`);
         fileId = link[link.length - 1];
+        console.debug(`${fileId}`)
     }
-    const widgetDownloadButton = document.querySelector("#__next > main > div.container.project-page > div.modal-container > section > div.actions > button");
+    const widgetDownloadButton = document.querySelector("#__next > div > main > div.container.project-page > div.modal-container > section > div.actions > button");
     if (widgetDownloadButton + fileId) {
         widgetDownloadButton.addEventListener('click', function () {
             const modpageUrl = window.location.href;
@@ -21,15 +24,18 @@ let handleWidgetAppearance = () => {
 };
 
 function getModId() {
-    let modId = document.querySelector("#__next > main > div.container.project-page > aside > div.aside-box.project-details-box > section:nth-child(2) > dl > dd:nth-child(6)");
+    let modId = document.querySelector("#__next > div > main > div.container.project-page > aside > div.aside-box.project-details-box > section:nth-child(2) > dl > dd:nth-child(6)");
     if (modId) {
         modId = modId.textContent.trim();
+        console.debug("mod id found" + modId);
     }
     return modId;
 }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    //if active tab changed -> get modId from page and send it back to serviceWorker
     if (message.reason === "activeTabChanged") {
+        console.debug("content script received message: active tab changed");
         let modId = getModId();
         (modId) ? sendResponse(modId) : sendResponse("noModIdFound")
     }
@@ -37,7 +43,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 let modId = getModId();
 (modId) ? chrome.storage.session.set({idfc_modId: modId}) : undefined;
-const widgetDownloadButtonParentElement = document.querySelector("#__next > main > div.container.project-page");
+const widgetDownloadButtonParentElement = document.querySelector("#__next > div > main > div.container.project-page");
 let mutationObserverConfig = {childList: true, subtree: true}
 let callback = function (mutationsList, observer) {
     for (let mutation of mutationsList) {
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -16,10 +16,15 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     }
 });
 
-chrome.tabs.onActivated.addListener(async function (activeInfo) {
+chrome.tabs.onActivated.addListener(async function () {
+    //get the current tab
     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    //send msg to contentScript to retrieve modId
     const response = await chrome.tabs.sendMessage(tab.id, {reason: "activeTabChanged"});
+    //if a modId has been found
     if (response !== "noModIdFound") {
+        console.debug("service Worker received response from content script, from message with reason 'active tab changed' plus MOD ID HAS BEEN FOUND" + response);
+        //create redirect for instant download
         await chrome.declarativeNetRequest.updateSessionRules({
             addRules: [{
                 "id": await generateRandomId(), "priority": 1, "action": {
