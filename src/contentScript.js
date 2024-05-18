3fd593599b8791dc5dcd68cde422ef3442f2579d
elias <elias>
2024-05-18 05:47:24 +0200

    update

--- src/contentScript.js
+++ src/contentScript.js
@@ -14,3 +14,33 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.error(`IDFC Extension ERROR: ${error}`);
     }
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const widgetDownloadButtonParentElement = document.querySelector("#__next > div > main > div.container.project-page");
+let mutationObserverConfig = {childList: true, subtree: true}
+let callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            handleWidgetAppearance();
+        }
+    }
+};
+let mutationObserver = new MutationObserver(callback);
+mutationObserver.observe(widgetDownloadButtonParentElement, mutationObserverConfig);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// let modId = getModId();
+// (modId) ? chrome.storage.session.set({idfc_modId: modId}) : undefined;
+// const widgetDownloadButtonParentElement = document.querySelector("#__next > div > main > div.container.project-page");
+// let mutationObserverConfig = {childList: true, subtree: true}
+// let callback = function (mutationsList, observer) {
+//     for (let mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             handleWidgetAppearance();
+//         }
+//     }
+// };
+// let mutationObserver = new MutationObserver(callback);
+// mutationObserver.observe(widgetDownloadButtonParentElement, mutationObserverConfig);
